abec014e3dd5ed5aa61fc488e3683196a9a86f17
MadaMarginean <madalina_marginean96@yahoo.com>
2018-04-02 21:08:17 +0300

    App

--- src/test/java/StudentsLaboratoriesMV/AppTest.java
+++ src/test/java/StudentsLaboratoriesMV/AppTest.java
@@ -4,11 +4,15 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
+        extends TestCase
 {
     /**
      * Create the test case
